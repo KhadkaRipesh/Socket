3924d34ec3492161afc38007854e07cac2a83e95
KhadkaRipesh <khadkanripesh@gmail.com>
2023-08-31 19:38:37 +0545

    add modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ChatModule } from './chat/chat.module';
+import { NotificationModule } from './notification/notification.module';
 
 @Module({
-  imports: [],
+  imports: [ChatModule, NotificationModule],
   controllers: [AppController],
   providers: [AppService],
 })
